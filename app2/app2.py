70e919ababccda1c012402699342f634d3e1eba1
Nguyen Nguyen <nltn@msn.com>
2023-02-18 09:50:32 -0800

    bigbig

--- app2/app2.py
+++ app2/app2.py
@@ -4,6 +4,15 @@ import schedule  # Scheduler
 import requests
 
 def task_1(text:str):
+    url = 'https://16b336b235e7f458c2cff4a428ef7780.m.pipedream.net'
+    data = {
+        "message": text
+    }
+
+    response = requests.post(url, json=data)
+    print("response= ", response.json())
+
+def task_2(text:str):
     print(text)
 #     url = 'https://16b336b235e7f458c2cff4a428ef7780.m.pipedream.net'
 #     data = {
@@ -13,11 +22,12 @@ def task_1(text:str):
 #     response = requests.post(url, json=data)
 #     print("response= ", response.json())
 
-schedule.every().minute.at(":00").do(task_1, "App#2: Hello World!!!")
+
+schedule.every().minute.at(":00").do(task_2, "Application#2: Hello World")
 # schedule.run_all()  # run
 
 dt = datetime.datetime.now()
-task_1(f"Application started at {dt.strftime('%Y-%m-%d %H:%M:%S')}")
+task_1(f"Application#2 started at {dt.strftime('%Y-%m-%d %H:%M:%S')}")
 
 while True:
     schedule.run_pending()
