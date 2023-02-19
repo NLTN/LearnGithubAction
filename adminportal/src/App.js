07af7666b6ff695964a8f05f2621e4b90f47d40d
Nguyen Nguyen <nltn@msn.com>
2023-02-19 00:53:39 -0800

    666666

--- adminportal/src/App.js
+++ adminportal/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React#aacaCdc
+          Learn React#aacaCdcaa
         </a>
       </header>
     </div>
