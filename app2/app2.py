d3ad027306e8bc1c74d789aa33715b96f12dc637
Nguyen Nguyen <nltn@msn.com>
2023-02-20 19:06:59 -0800

    ny

--- app2/app2.py
+++ app2/app2.py
@@ -27,7 +27,7 @@ schedule.every().minute.at(":00").do(task_2, "Application#2: Hello World")
 # schedule.run_all()  # run
 
 dt = datetime.datetime.now()
-task_1(f"Application ##2NEW on ProCluster started at {dt.strftime('%Y-%m-%d %H:%M:%S')}")
+task_1(f"Application ##2NEWYEAR on ProCluster started at {dt.strftime('%Y-%m-%d %H:%M:%S')}")
 
 while True:
     schedule.run_pending()
