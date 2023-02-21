79e3fdadf83a12b4c76e9200cf1e3b88aa40d8c1
Nguyen Nguyen <nltn@msn.com>
2023-02-20 19:36:10 -0800

    y

--- app1/app1.py
+++ app1/app1.py
@@ -27,7 +27,7 @@ schedule.every().minute.at(":00").do(task_2, "Application#001: Hello World")
 # schedule.run_all()  # run
 
 dt = datetime.datetime.now()
-task_1(f"Application #1 started at {dt.strftime('%Y-%m-%d %H:%M:%S')}")
+task_1(f"Application #1YEAH started at {dt.strftime('%Y-%m-%d %H:%M:%S')}")
 
 while True:
     schedule.run_pending()
